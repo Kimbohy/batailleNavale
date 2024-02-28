d4b9cd9b0c7b2bac8cdb015c79fa5a6a3423b54c
kimbohy <lovatina123@gmail.com>
2024-02-28 21:30:06 +0300

    first

--- main.cpp
+++ main.cpp
@@ -3,11 +3,7 @@
 
 using namespace std;
 
-struct Boat
-{
-    int size = 0;
-    char orientetion = 'e'; // North = n, South = s, West = w, East, e
-};
+
 
 using Boat = struct Boat;
 
--- point.cpp
+++ point.cpp
@@ -1,2 +1,8 @@
 #include "point.hpp"
 #include <iostream>
+
+struct Boat
+{
+    int size = 0;
+    char orientetion = 'e'; // North = n, South = s, West = w, East, e
+};
